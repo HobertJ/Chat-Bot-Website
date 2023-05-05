e077774a7d59ac310b2a50eb788a52199ce61ec2
shidqizh <shidqizh@gmail.com>
2023-05-05 22:11:52 +0700

    new chat

--- src/react-website/src/components/NewChat.js
+++ src/react-website/src/components/NewChat.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css"
-import "./NewChat.css"
+import React, { useState, useEffect } from 'react';
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import "./NewChat.css";
 
 function NewChat() {
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    if (refresh) {
+      window.location.reload();
+    }
+  }, [refresh]);
+
+  const handleRefresh = () => {
+    setRefresh(true);
+  };
+
   return (
     <div className="newchat-container position-absolute top-0 start-0 p-3">
-        <div>
-            <button className="newchat-button">
-                <i className="bi bi-file-plus"></i>
-                <span className='newchat-button-text'>New Chat</span>
-            </button>
-            
-        </div>
+      <div>
+        <button className="newchat-button" onClick={handleRefresh}>
+          <i className="bi bi-file-plus"></i>
+          <span className='newchat-button-text'>New Chat</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NewChat
+export default NewChat;
